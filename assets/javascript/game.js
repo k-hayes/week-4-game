6df93cd66e9194af67c0c1d7a16fc688e777c8e5
Kate Hayes <kate.beaver@gmail.com>
2017-07-22 12:08:37 -0500

    Something more complete.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,50 @@
+$( document ).ready(function(){
+	
+	var randomNumber = Math.floor(Math.random() * 100);
+		$("#random").text(number);
 
-var wins = 0;
-var losses = 0;
-var counter = [];
+	var crystalNumber1 = Math.floor(Math.random() * 10);
+		$("#Crystal-1").on("click" function(){
+			counter = counter + crystalNumber1;
+			console.log("New counter= " + counter)
+		})
+	var crystalNumber2 = Math.floor(Math.random() * 10);
+		$("#Crystal-2").on("click" function(){
+			counter = counter + crystalNumber2;
+			console.log("New counter= " + counter)
+		})	
+	var crystalNumber3 = Math.floor(Math.random() * 10);
+		$("#Crystal-3").on("click" function(){
+			counter = counter + crystalNumber3;
+			console.log("New counter= " + counter)
+		})
+	var crystalNumber4 = Math.floor(Math.random() * 10);
+		$("#Crystal-4").on("click" function(){
+			counter = counter + crystalNumber4;
+			console.log("New counter= " + counter)
+		})
 
-function randomNumber(){
-	var number = Math.floor(Math.random() * 100);
-	$("#random").text(number)
-}
-
-function crystalNumber(){
-	var clickNumber = Math.floor(Math.random() * 10);
-	$(".Crystal-Button").on("click", function(){
-	counter = counter + parseInt($(this).data("num"))	
-}
+	var wins = 0;
+	var losses = 0;
+	var counter = 0;
 
 
+	$("#wins").text(wins);
+	$("#losses").text(losses);
 
 
-
-
-
-
-
-
-if(counter === randomNumber){
+	if(counter === randomNumber){
 		wins++;
 		counter = [];
 		randomNumber = Math.floor(Math.random() * 100);
 		crystalNumber = Math.floor(Math.random() * 10);
 	}
-if(counter > randomNumber)
+	
+	else if(counter > randomNumber)
 		losses++;
 		counter = [];
 		randomNumber = Math.floor(Math.random() * 100);
 		crystalNumber = Math.floor(Math.random() * 10);
 	}
+
+}
