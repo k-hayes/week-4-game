d36efab20f834d6c4bbeab9b4834c2bf9f877d1a
Kate Hayes <kate.beaver@gmail.com>
2017-07-26 20:30:06 -0500

    Actual working files thanks to Everett.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-  var randomNumber=Math.floor(Math.random()*100)
+  var randomNumber=Math.floor(Math.random()* 50)
 
   $("#random").text(randomNumber);
 
-  var num1= Math.floor(Math.random()*10)
-  var num2= Math.floor(Math.random()*10)
-  var num3= Math.floor(Math.random()*10)
-  var num4= Math.floor(Math.random()*10)
+  var crystalNum1= Math.floor(Math.random()*10)
+  var crystalNum2= Math.floor(Math.random()*10)
+  var crystalNum3= Math.floor(Math.random()*10)
+  var crystalNum4= Math.floor(Math.random()*10)
 
   var countTotal= 0; 
   var wins= 0;
@@ -16,15 +16,14 @@ $("#wins").text(wins);
 $("#losses").text(losses);
 
 function gameReset(){
-      randomNumber = Math.floor(Math.random()* 100);
-      console.log(Random)
-      $("#random").text(randomNumber);
+      randomNumber = Math.floor(Math.random()* 50);
+        $("#random").text(randomNumber);
       crystalNum1 = Math.floor(Math.random()*10);
       crystalNum2 = Math.floor(Math.random()*10);
       crystalNum3 = Math.floor(Math.random()*10);
       crystalNum4 = Math.floor(Math.random()*10);
       countTotal = 0;
-      $("#counter").text(countTotal);
+      $("#counter").html(countTotal);
       } 
 
 
@@ -32,51 +31,59 @@ function gameReset(){
     countTotal = countTotal + crystalNum1;
     $("#counter").text(countTotal); 
 
-        if (countTotal == randomNumber){
+        if (countTotal === randomNumber){
           wins++;
           alert("You win!");
+          gameReset();
         }
         else if ( countTotal > randomNumber){
           losses++;
-          alert("You lose! You're a big loser!")
+          alert("You lose! You're a big loser!");
+          gameReset();
         }   
   })  
   $("#Crystal-2").on ('click', function(){
     countTotal = countTotal + crystalNum2;
     $("#counter").text(countTotal); 
-        if (countTotal == randomNumber){
+        if (countTotal === randomNumber){
           wins++;
           alert("You win!");
+          gameReset();
         }
         else if (countTotal > randomNumber){
           losses++;
-          alert("You lose! You're a big loser!")
+          alert("You lose! You're a big loser!");
+          gameReset();
         }   
   })  
   $("#Crystal-3").on ('click', function(){
     countTotal = countTotal + crystalNum3;
     $("#counter").text(countTotal);
 
-         if (countTotal == randomNumber){
+         if (countTotal === randomNumber){
           wins++;
           alert("You win!");
+          gameReset();
         }
         else if (countTotal > randomNumber){
           losses++;
-          alert("You lose! You're a big loser!")
+          alert("You lose! You're a big loser!");
+          gameReset();
         }   
   })  
   $("#Crystal-4").on ('click', function(){
     countTotal = countTotal + crystalNum4;
     $("#counter").text(countTotal); 
       
-        if (countTotal == randomNumber){
+        if (countTotal === randomNumber){
           wins++;
           alert("You win!");
+          gameReset();
         }
         else if (countTotal > randomNumber){
           losses++;
-          alert("You lose! You're a big loser!")
+          alert("You lose! You're a big loser!");
+          gameReset();
         }   
   });   
 }); 
